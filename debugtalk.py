190bfe5897dbebb51baabe5360e4d1ff53db87d7
zousongbai <994580004@qq.com>
2020-07-29 17:46:11 +0800

    day727：参数化（variables参数化）、环境变量（.env文件定义）、动态参数、关联接口的应用（testcases）

--- debugtalk.py
+++ debugtalk.py
@@ -1,5 +1,21 @@
-
+import random
 import time
+
 
 def sleep(n_secs):
     time.sleep(n_secs)
+
+
+def get_random_user_agent():
+    """获取随机的user_agent"""
+    users_agent = ['Mozilla/5.0 Ally01', 'Mozilla/5.0 Ally02',
+                  'Mozilla/5.0 Ally03', 'Mozilla/5.0 Ally04,'
+                  ]
+    # random.choice：随机选择
+    u_agent = random.choice(users_agent)
+    print(u_agent)
+    return u_agent
+
+
+if __name__ == '__main__':
+    get_random_user_agent()
